15b5eb7f01a8120f5948e9aa16c5d5da3ad81dc5
nstuder <niki.studer@web.de>
2023-04-11 20:49:39 +0200

    add views and styles

---
+++ src/client/main.ts
@@ -0,0 +1,6 @@
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/tab'
