a907b572294dc7565063b0e7c6c55a72c482a51a
nstuder <niki.studer@web.de>
2023-05-14 15:49:31 +0200

    add deeplinks for Tabs

--- src/client/main.ts
+++ src/client/main.ts
@@ -3,4 +3,21 @@ import 'bootstrap/js/dist/carousel'
 import 'bootstrap/js/dist/collapse'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/tab'
+import Tab from 'bootstrap/js/dist/tab'
+
+var hash = decodeURI(new URL(location.href).hash)
+if (location.hash) {
+    var currentTab = document.querySelector('[href="' + hash + '"]');
+    var curTab = Tab.getOrCreateInstance(currentTab);
+    curTab.show();
+}
+
+var url = location.href.replace(/\/$/, '');
+var selectableTabList = [].slice.call(document.querySelectorAll('a[data-bs-toggle="tab"]'));
+selectableTabList.forEach((selectableTab) => {
+  selectableTab.addEventListener('click', function () {
+    var hash = selectableTab.getAttribute('href');
+    var newUrl = url.split('#')[0] + hash;
+    history.replaceState(null, null, newUrl);
+  });
+});
