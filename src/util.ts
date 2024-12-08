e4aa93ea9867fdb8173f1518a8685777a5572d4d
nstuder <niki.studer@web.de>
2024-12-08 17:41:24 +0100

    linting && accessebillity fixes

--- src/util.ts
+++ src/util.ts
@@ -1,43 +1,43 @@
 import payload from 'payload'
 
 export const withContext = async (data) => {
-    return {
-        ...data,
-        ...{
-            global: {
-                navigation: await payload.findGlobal({
-                    slug: 'navigation',
-                    locale: 'de',
-                }),
-                configuration: await payload.findGlobal({
-                    slug: 'configuration',
-                    locale: 'de',
-                }),
-            },
-        },
-    }
+	return {
+		...data,
+		...{
+			global: {
+				navigation: await payload.findGlobal({
+					slug: 'navigation',
+					locale: 'de',
+				}),
+				configuration: await payload.findGlobal({
+					slug: 'configuration',
+					locale: 'de',
+				}),
+			},
+		},
+	}
 }
 
 export const withMeta = async (context, data) => {
-    return {
-        ...(await context),
-        meta: {
-            ...(await payload.findGlobal({ slug: 'seo', locale: 'de' })),
-            ...data,
-        },
-    }
+	return {
+		...(await context),
+		meta: {
+			...(await payload.findGlobal({ slug: 'seo', locale: 'de' })),
+			...data,
+		},
+	}
 }
 
 export const hbsHelpers = {
-    eq: (a, b, options) => {
-        if (a === b) {
-            return options.fn(this)
-        }
-        return options.inverse(this)
-    },
-    toLocalDate: (a: string) => {
-        return new Date(a).toLocaleDateString('de-DE', {
-            timeZone: 'Europe/Berlin',
-        })
-    },
+	eq: (a, b, options) => {
+		if (a === b) {
+			return options.fn(this)
+		}
+		return options.inverse(this)
+	},
+	toLocalDate: (a: string) => {
+		return new Date(a).toLocaleDateString('de-DE', {
+			timeZone: 'Europe/Berlin',
+		})
+	},
 }
